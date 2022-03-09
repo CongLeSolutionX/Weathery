dd1796d3e0d8a79b520bdcc6ef159422034afa41
Cong Le <longchike@gmail.com>
2022-03-09 11:03:07 -0800

    Initial setup

---
+++ Weathery/Weathery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weathery
+//
+//  Created by Cong Le on 3/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
